395cc5c48ea48c426bcfe6884d87f4e001b4cad6
Ramana7125 <24100010035.uset@ltsu.ac.in>
2025-06-26 15:48:13 +0530

    Add files via upload

---
+++ ibm.26-6-25.1.py
@@ -0,0 +1,14 @@
+tuple_1=("ram","teja","krishna","ramana","loki","raju","rocky")
+tuple_2=(1022,2354,9856,4257,420,457,819)
+
+
+user_name=input("enter your user id:: ")
+user_password = int(input("enter your password :: "))
+if tuple_1:
+    index = tuple_1.index(user_name)
+    if tuple_2[index] == user_password:
+        print("ligin succesfully completed.  welcome to.......")
+    else:
+        print("user password is wrong")
+else:
+    print("user name is wrong")
